4d4f694fc39ac723e85f5db184e8fe488e42b364
Abraham <faustabraham@gmail.com>
2024-05-23 22:34:46 +0300

    remove old files

--- js/setting.js
+++
@@ -1,7 +0,0 @@
-
-// here you can change some settings
-
-export const numberOfFloors = 20;
-export const numberOfElevators = 4;
-export const floorHeight = '103px' // make sure to change the int height too
-export const floorHeightInt = 103
